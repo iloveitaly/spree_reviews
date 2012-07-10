3ce8027a723bd14b4e9c4d743f13b413b9580add
Johan Bruning <johan.h.bruning@gmail.com>
2012-07-10 18:26:18 +0200

    Fix for Rails security update. Need to make those attributes accessible

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -3,6 +3,8 @@ Spree::Product.class_eval do
   attr_accessible :avg_rating, :reviews_count
   has_many :reviews
 
+  attr_accessible :avg_rating, :reviews_count
+
   def stars
     avg_rating.round
   end
